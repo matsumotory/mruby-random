c39798f7e6c0844b08a64f7da6813d420eb88c84
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-28 23:18:32 +0900

    example changed

--- example/random.rb
+++ example/random.rb
@@ -1,6 +1,12 @@
 r = Random.new();
 
-r.srand()
+r.srand(10)
+puts r.rand()
+r.srand(20)
+puts r.rand()
+r.srand(10)
+puts r.rand()
+
 100.times do |i|
   puts r.rand()
   puts r.rand(0)
@@ -8,3 +14,4 @@ r.srand()
   puts r.rand(50)
   puts r.rand(100)
 end
+
